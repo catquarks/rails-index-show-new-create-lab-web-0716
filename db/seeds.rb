37da140a16cdfe9857f7caec888bc09ffab51b3e
Tami <catquarks@gmail.com>
2016-08-18 01:55:58 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Coupon.create(coupon_code: "Lala", store: "EZ Mart")
+Coupon.create(coupon_code: "Dodo", store: "Quicky Mart")
+Coupon.create(coupon_code: "Kaka", store: "Poom Store Mart")
+Coupon.create(coupon_code: "Juju", store: "Bongoes Store")
+Coupon.create(coupon_code: "Durrr", store: "NYC Bodega")
+Coupon.create(coupon_code: "Pork", store: "Pongamonga")
+Coupon.create(coupon_code: "Huuuur", store: "A store Mart")
